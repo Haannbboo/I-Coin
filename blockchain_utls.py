34ca082a40949864e1d1be5cc1e196d8994cc0d7
Hanbo <wanliuguohanbo@163.com>
2019-11-12 07:45:09 +0800

    Update

---
+++ blockchain_utls.py
@@ -0,0 +1,38 @@
+import requests
+import PySimpleGUI as sg
+import hashlib as hl
+from urllib import urlencode
+import socket
+import rsa
+
+
+class DynamicMain:
+    ip = socket.gethostbyname(socket.gethostname())
+    def __init__(self):
+        self.ip =  if 'http://' in ip else 'http://'+ip
+
+    def Mine(self):
+        _, code = requests.get('/'.join(self.ip, 'mine'))
+        if code == 200:
+            return
+
+    def Register(self):
+        public, private = rsa.newkeys(1024)
+        self.account = public
+        # data = 
+        _, code = requests.post('/'.join(self.ip, 'nodes', 'register'), json=data)
+        if code == 201:
+            return private
+
+    def Transaction(self, recipient=None, amount=None, key=None):
+        sender = self.account
+        params = {'sender': sender, 'recipient': recipient, 'amount': amount}
+        # check signature
+        # check amount
+        _, code = requests.post('/'.join(self.ip, 'transactions/new'),
+                                json = params)
+        if code == 400:
+            return "Missing values"
+
+    def _check_signature(self, key):
+        pass
