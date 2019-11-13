8955ea1cf02294a833c582344b8c5b6a2329c4d5
Hanbo <wanliuguohanbo@163.com>
2019-11-13 09:24:29 +0800

    add transaction authorization

--- blockchain_utls.py
+++ blockchain_utls.py
@@ -1,6 +1,5 @@
 import requests
 import hashlib as hl
-from urllib import urlencode
 import socket
 import rsa
 
@@ -8,7 +7,8 @@ import rsa
 class DynamicMain:
     ip = socket.gethostbyname(socket.gethostname())
     def __init__(self):
-        self.ip =  if 'http://' in ip else 'http://'+ip
+        self.ip = ip if 'http://' in ip else 'http://'+ip
+        self.public_key = None
 
     def Mine(self):
         _, code = requests.get('/'.join(self.ip, 'mine'))
@@ -21,17 +21,27 @@ class DynamicMain:
         # data = 
         _, code = requests.post('/'.join(self.ip, 'nodes', 'register'), json=data)
         if code == 201:
-            return private
+            self.private = private
 
-    def Transaction(self, recipient=None, amount=None, key=None):
+    def Transaction(self, recipient=None, amount=None, private_key=None):
         sender = self.account
         params = {'sender': sender, 'recipient': recipient, 'amount': amount}
-        # check signature
+        _, code = self._authorize_transaction(params, private_key)
         # check amount
         _, code = requests.post('/'.join(self.ip, 'transactions/new'),
                                 json = params)
         if code == 400:
             return "Missing values"
 
-    def _check_signature(self, key):
-        pass
+    def _authorize_transaction(self, trans_msg, private_key):
+        if self.public_key is None:
+            return "Missing public key", 400
+
+        signature = rsa.sign(trans_msg.encode(), private_key, 'SHA-256')
+        try:
+            rsa.verify(trans_msg.encode(), signature, self.public_key)
+            return "Verify succeed", 200
+        except rsa.pkcs1.VerificationError:
+            return "Verify failed", 502
+
+        
